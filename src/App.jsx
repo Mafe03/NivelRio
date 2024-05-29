5ce878a4ada2ae0fa8c57f9575d246b263a0aa4d
unknown <angiergiraldo12@gmail.com>
2024-05-29 07:55:00 -0500

    firebase 1.o

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import Routing from './router/Routing'
+// src/App.jsx
+import React, { useEffect } from 'react';
+import Routing from './router/Routing';
+import { getToken, onMessage } from 'firebase/messaging';
+import { messaging } from './firebase/firebaseConfig';  // Asegúrate de que la ruta es correcta
+import { toast, ToastContainer } from 'react-toastify';
+import Message from './components/Message';  // Asegúrate de que la ruta es correcta
+import 'react-toastify/dist/ReactToastify.css';
 
+const { VITE_APP_VAPID_KEY } = import.meta.env;
+
+async function requestPermission() {
+  const permission = await Notification.requestPermission();
+  if (permission === 'granted') {
+    try {
+      const token = await getToken(messaging, { vapidKey: VITE_APP_VAPID_KEY });
+      console.log('Token generated: ', token);
+    } catch (error) {
+      console.error('Error getting token: ', error);
+    }
+  } else if (permission === 'denied') {
+    alert('You denied the notification');
+  }
+}
 
 function App() {
+  useEffect(() => {
+    requestPermission();
+
+    const unsubscribe = onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      toast(<Message notification={payload.notification} />);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <>
-    <Routing/>
+      <ToastContainer />
+      <Routing />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,28 +1,26 @@
-import React, { useState } from 'react';
-import { Routes, Route, BrowserRouter  } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import LayoutDash from '../components/Dashboard/LayoutDash';
 import Inicio from '../components/Dashboard/Inicio';
-import { AuthProvider } from '../context/AuthProvide';
-import Layaout from '../components/Public/Layout';
+import { AuthProvider } from '../context/AuthProvide.jsx'; // Verifica que el nombre del archivo sea correcto
+import Layout from '../components/Public/Layout'; // Asegúrate de que el nombre del componente sea correcto
 import Login from '../components/Public/Login';
 
-
 const Routing = () => {
-    return ( <>
+  return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Layaout />}>
+          <Route path="/" element={<Layout />}>
             <Route index element={<Login />} />
           </Route>
-          <Route path="/Dashboard" element={<LayoutDash />}>
+          <Route path="/dashboard" element={<LayoutDash />}>
             <Route index element={<Inicio />} />
-            
           </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
-    </> );
+  );
 }
- 
+
 export default Routing;
